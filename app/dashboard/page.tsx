532a744ff503bdf43c047c98576b7c9025cdf71e
joon <21-1-00002@vsu.edu.ph>
2026-04-12 19:24:26 +0800

    Add dashboard page
    added dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      {}
+      <h1 className="text-2xl font-semibold text-gray-800">
+        Dashboard Content Coming Soon!
+      </h1>
+    </div>
+  );
+}
